970043e4e80b33a98192206cd6fbc8d14c2de1fc
Stampanone <blackqueen_nyj@hotmail.com>
2023-08-03 09:26:55 -0300

    task 1

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,56 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String dni;
+    private String name;
+    private String lastname;
+
+    public Client(){
+
+    }
+    public Client(String dni, String name, String lastname) {
+        this.dni = dni;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
